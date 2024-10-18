26a4642a0fc19ad557e47889c6f1b50304827e2b
ngochaupy <haulengoc4283@gmail.com>
2024-10-18 19:09:09 +0700

    s

--- src/java/huypn/util/DBHelper.java
+++ src/java/huypn/util/DBHelper.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
  *
  * @author hien
  */
+/*
 public class DBHelper {
     public static Connection getConnection()
         throws ClassNotFoundException,SQLException{
@@ -27,4 +28,41 @@ public class DBHelper {
         //4.return con
         return con;
     }
+}
+*/
+
+public class DBHelper {
+    
+
+    public static Connection getConnection(){
+        
+        String url;        
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        } else{
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        }
+        
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("PRJ301DEMO: Can not load JDBC Driver. Please check your pom file");
+        }
+        
+        try {
+            Connection con = DriverManager.getConnection(url, userID, password);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("PRJ301DEMO: Can not connect SQL Server. Reason: " + ex.getMessage());                        
+        }
+        return null;
+    }
+    
+    
+    private final static String serverName = "localhost";
+    private final static String dbName = "PRJMovieName";
+    private final static String portNumber = "1433";
+    private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final static String userID = "sa";
+    private final static String password = "12345";
 }
